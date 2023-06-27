fef17d8026ee4d69f1ad258de46394cf931f777b
jonghwa4077 <ehdxka3@naver.com>
2023-06-27 14:31:38 +0900

    styled component

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const StContainer = styled.div`
+  display: flex;
+`
+
+const StBox = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 1px solid ${(props) => props.borderColor};
+  margin: 20px;
+  background-color: ${(props) => props.backgroundColor};`
+
+  // 박스의 색
+  const boxList = ['red', 'blue', 'green', 'black']
+
+  // 색을 넣으면 이름을 반환
+
+  const getBoxName = (color) => {
+    switch (color) {
+      case 'red':
+        return '빨간 박스'
+      case 'green':
+        return '초록 박스'
+      case 'blue':
+        return '파란 박스'
+      default:
+        return '검정 박스'
+    }
+  }
+
+function App() {
+  return (
+    <StContainer>
+      <>
+        {/* <StBox borderColor="red">빨간 박스</StBox> */}
+        {
+          boxList.map((box)=>{
+            return <StBox borderColor={box}>{getBoxName}</StBox>
+          })
+        }
+      </>
+    </StContainer>
+  )
+}
+
+export default App
